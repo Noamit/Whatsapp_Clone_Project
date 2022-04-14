b50dc8057691da43328720caf2bb3e1b90f264e9
Noamit <noa1amit2@gmail.com>
2022-04-14 22:01:38 +0300

    noa

--- src/SingleSideChat.js
+++ src/SingleSideChat.js
@@ -7,8 +7,10 @@ function SingleSideChat({ name, lastMessage, lastseen }) {
         <p className='SingleSideChat'>
             <img id="b" src={img2} className="rounded-circle" alt="new contant" />
             <div className='SingleSideChat_info'>
+                <p className='a'>
                 <div id="info_name">{name} </div>
                 <div className="info_lastSeen">{lastseen}</div>
+                </p>
                 <div className="info_message">{lastMessage}</div>
             </div>
         </p>
