2d8b0ee580c4298e91e4ef3e827cf90bf5905b36
Noamit <noa1amit2@gmail.com>
2022-04-15 16:58:17 +0300

    noa

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Sidebar.css'
 import { PersonPlus } from 'react-bootstrap-icons'
 import img1 from './img1.jpg'
 import SingleSideChat from './SingleSideChat'
+import Modal from './Modal'
 
 function Sidebar() {
+
+  const [openModel, setOpenModel] = useState(false);
+
   return (
     <div className='sidebar'>
       <div className='sidebar_header'>
         <img src={img1} className="rounded-circle" alt="user" />
         <div id="userName">Noa Amit</div>
-        <button id='presonPlus_button'>
+        <button id='presonPlus_button' data-bs-toggle="modal" data-bs-target="#mymodal" onClick={() => {setOpenModel(true); }}>
           <PersonPlus />
         </button>
       </div>
@@ -22,9 +26,8 @@ function Sidebar() {
         <SingleSideChat name="tomer" lastMessage={"loveyouuuuuhuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu "} lastseen="03:54" />
         <SingleSideChat name="tomer" lastMessage={"loveyouuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu "} lastseen="03:54" />
         <SingleSideChat name="tomer" lastMessage={"noa "} lastseen="03:54" />
-
-
       </div>
+      {openModel && <Modal close_modal={setOpenModel} />}
     </div>
   )
 }
