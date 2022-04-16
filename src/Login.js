624014b9b423c101a1090e7ec4bf9c603e722351
Noamit <noa1amit2@gmail.com>
2022-04-16 17:45:21 +0300

    noa1

--- src/Login.js
+++ src/Login.js
@@ -1,50 +1,55 @@
-import React, {useState} from 'react';
-import Appbody from './Appbody';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Login.css'
 
-
 function Login() {
 
-    // const [userInfo, setUserInfo] = useState ({userName: "", password: ""});
+    const [isLoggedIn, setIsLoggedIn] = useState(true);
 
-    // const validation = (e) => {
-    //     return true;
-    // }
-
+    const renderAuthButton = () => {
+        if (isLoggedIn) {
+            return (
+                <Link to='/chat'>
+                    <input type="submit" value="login" className="btn btn-outline-secondary" />
+                </Link>
+            );
+        } else {
+            return (
+                <input type="submit" value="login" className="btn btn-outline-secondary" />
+            );
+        }
+    }
     return (
         <div className='login'>
-                <div className='login_page'>
-                    <div className='login_header'>
-                        NTM
-                    </div>
-                    <div className='login_body'>
-                        <h2> Login </h2>
-                        <form id="loginForm" className="form">
-                            <div className='login_body_input'>
-                                <input placeholder="Username" className="login_input" type="text" />
-                            </div>
-                            <div className='login_body_input'>
-                                <input placeholder="Password"  className="login_input" type="password"/>
-                            </div>
-                            <div className='login_buttom'>
-                                {/* <Link to='/chat'> */}
-                                <input type="submit" value="login" className="btn btn-outline-secondary" />
-                                {/* </Link> */}
-                            </div>
-                        </form>
-
-                        <div id='notregistered'>Not registered?&nbsp;
-                            <span>
-                                <Link to='/register' id="forRegister">
-                                    click here
-                                </Link>
-                                &nbsp;to Register.
-                            </span>
+            <div className='login_page'>
+                <div className='login_header'>
+                    NTM
+                </div>
+                <div className='login_body'>
+                    <h2> Login </h2>
+                    <form id="loginForm" className="form" >
+                        <div className='login_body_input'>
+                            <input placeholder="Username" className="login_input" type="text" />
                         </div>
+                        <div className='login_body_input'>
+                            <input placeholder="Password" className="login_input" type="password" />
+                        </div>
+                        <div className='login_buttom'>
+                            {renderAuthButton()}
+                        </div>
+                    </form>
+
+                    <div id='notregistered'>Not registered?&nbsp;
+                        <span>
+                            <Link to='/register' id="forRegister">
+                                click here
+                            </Link>
+                            &nbsp;to Register.
+                        </span>
                     </div>
                 </div>
-            </div>
+            </div >
+        </div >
     );
 }
 
