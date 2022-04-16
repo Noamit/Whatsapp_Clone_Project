8c3b8b5c28c1c48ff93963a16b31917c700fb7ab
Noamit <noa1amit2@gmail.com>
2022-04-16 14:13:53 +0300

    npa

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Chat.css'
 import img1 from './photo.jpg'
-import { Paperclip } from 'react-bootstrap-icons'
+import { Paperclip, CameraReels, Mic, Image} from 'react-bootstrap-icons'
 
 
 function Chat() {
@@ -9,10 +9,10 @@ function Chat() {
     <div className='chat'>
 
       <div className='chat_header'>
-      <img src={img1} id="rounded-circle_chat" className="rounded-circle" alt='user' />
+        <img src={img1} id="rounded-circle_chat" className="rounded-circle" alt='user' />
         <div className='header_info'>
-           <div id='header_info_name' >Tomer</div>
-           <p>Last seen at....</p>
+          <div id='header_info_name' >Tomer</div>
+          <p>Last seen at....</p>
         </div>
       </div>
       <div className='chat_body'>
@@ -23,7 +23,19 @@ function Chat() {
       </div>
 
       <div className='chat_input'>
-        <button><Paperclip /></button>
+        <div className="btn-group dropup">
+          <button id="Paperclip_button" type="button" data-bs-toggle="dropdown" aria-expanded="false" >
+          <Paperclip />
+          </button>
+          <ul className="dropdown-menu">
+            <div className='buttons_optaions'>
+              <button>&nbsp;<CameraReels />&nbsp;</button>
+              <button>&nbsp;<Mic />&nbsp;</button>
+              <button>&nbsp;<Image />&nbsp;</button>
+            </div>
+          </ul>
+        </div>
+
         <form>
           <input placeholder='Type a Message' />
           <button type='submit'>Send</button>
