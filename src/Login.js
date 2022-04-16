86e987d4e4504d2efe2ad1570e2e0683bc6fc6b0
Noamit <noa1amit2@gmail.com>
2022-04-16 15:59:29 +0300

    NOA\

--- src/Login.js
+++ src/Login.js
@@ -1,40 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Appbody from './Appbody';
 import { Link } from 'react-router-dom';
 import './Login.css'
 
-class Login extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.SubmitHandler = this.onSubmitHandler.bind(this);
-    }
+function Login() {
 
-    onSubmitHandler = (e) => {
-        e.preventDefault();
-        document.getElementById("loginForm").submit();
+    // const [userInfo, setUserInfo] = useState ({userName: "", password: ""});
 
-    }
+    // const validation = (e) => {
+    //     return true;
+    // }
 
-    render() {
-        return (
-            <div className='login'>
+    return (
+        <div className='login'>
                 <div className='login_page'>
                     <div className='login_header'>
                         NTM
                     </div>
                     <div className='login_body'>
                         <h2> Login </h2>
-
-                        <form id="loginForm" className="form" action="/chat">
+                        <form id="loginForm" className="form">
                             <div className='login_body_input'>
                                 <input placeholder="Username" className="login_input" type="text" />
                             </div>
                             <div className='login_body_input'>
-                                <input placeholder="Password" className="login_input" type="password" />
+                                <input placeholder="Password"  className="login_input" type="password"/>
                             </div>
-
                             <div className='login_buttom'>
-                                <input type="submit" value="login" className="btn btn-outline-secondary" onClick={this.onSubmitHandler} />
+                                {/* <Link to='/chat'> */}
+                                <input type="submit" value="login" className="btn btn-outline-secondary" />
+                                {/* </Link> */}
                             </div>
                         </form>
 
@@ -49,8 +45,7 @@ class Login extends React.Component {
                     </div>
                 </div>
             </div>
-        );
-    }
+    );
 }
 
 export default Login;
