837f5faa830b32c202316000a82c5694ea106d7e
Tomerki <Tomerh1810@gmail.com>
2022-04-19 22:54:41 +0300

    tomer

--- src/DataBase.js
+++ src/DataBase.js
@@ -17,6 +17,6 @@ const dataBase = new DataBase();
 dataBase.addUserToDataBase('noamit', new User('noamit', 'noa amit', 'noa1', noadog));
 dataBase.addUserToDataBase('tomer',new User('tomer', 'tomer hadar', 'tomer1', noadog));
 dataBase.addUserToDataBase('noamit1', new User('noamit', 'noa amit', 'noa1', noadog));
-dataBase.addUserToDataBase('tomer2',new User('tomer', 'tomer hadar', 'tomer1', noadog));
+dataBase.addUserToDataBase('tomer2',new User('tomer', 'tomer_hadar', 'tomer1', noadog));
 
 export default dataBase;
