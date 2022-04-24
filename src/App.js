ebdb557f839821b033d51b55e62e080d2daa9270
Noamit <noa1amit2@gmail.com>
2022-04-24 21:46:02 +0300

    real_end

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <Routes>
           <Route path='/' element={<Login userSetter={setNewUser} />}></Route>
           <Route path='/register' element={<Register />}></Route>
-            <Route path='/chat' element={<Realod userSetter={setNewUser} />}></Route>
+          <Route path='/chat' element={<Realod userSetter={setNewUser} />}></Route>
           </Routes>
         </BrowserRouter>
       );
